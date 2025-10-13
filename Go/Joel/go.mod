63d510443dd61de5c68d041d7e76c53543872979
PrinceNoir23 <joelsebastianrh22@gmail.com>
2025-10-12 21:40:26 -0500

    go mod path

--- Go/Joel/go.mod
+++ Go/Joel/go.mod
@@ -1,3 +1,3 @@
-module github.com/PrinceNoir23/IntroToAlgorithms
+module github.com/PrinceNoir23/IntroToAlgorithms/Go/Joel
 
 go 1.25.1
--- Go/Joel/main.go
+++ Go/Joel/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	ex "github.com/PrinceNoir23/IntroToAlgorithms/ch01/ex"
-	// alg "github.com/PrinceNoir23/IntroToAlgorithms/Joel/ch01/algorithms"
-	// prob "github.com/PrinceNoir23/IntroToAlgorithms/Joel/ch01/prob"
+	ex "github.com/PrinceNoir23/IntroToAlgorithms/Go/Joel/ch01/ex"
+	// alg "github.com/PrinceNoir23/IntroToAlgorithms/Go/Joel/ch01/algorithms"
+	// prob "github.com/PrinceNoir23/IntroToAlgorithms/Go/Joel/ch01/prob"
 )
 
 func main() {
