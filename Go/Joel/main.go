1bdc6dc6ec13051812ee89f9def5f6ec6912c508
PrinceNoir23 <joelsebastianrh22@gmail.com>
2025-10-12 21:13:10 -0500

    go mod path

--- Go/Joel/main.go
+++ Go/Joel/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	ex "github.com/PrinceNoir23/IntroToAlgorithms/Joel/ch01/ex"
+	ex "github.com/PrinceNoir23/IntroToAlgorithms/Go/Joel/ch01/ex"
 	// alg "github.com/PrinceNoir23/IntroToAlgorithms/Joel/ch01/algorithms"
 	// prob "github.com/PrinceNoir23/IntroToAlgorithms/Joel/ch01/prob"
 )
--- Go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/PrinceNoir23/IntroToAlgorithms
-
-go 1.25.1
